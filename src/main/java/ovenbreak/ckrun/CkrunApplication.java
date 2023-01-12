b13ce03aaedeb4828d2f3563a36cae8abcd6a19d
jujinyoung <jujinyoung1838@naver.com>
2023-01-12 16:16:38 +0900

    프로젝트 생성

---
+++ src/main/java/ovenbreak/ckrun/CkrunApplication.java
@@ -0,0 +1,13 @@
+package ovenbreak.ckrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkrunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkrunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ovenbreak/ckrun/CkrunApplicationTests.java
@@ -0,0 +1,13 @@
+package ovenbreak.ckrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkrunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
