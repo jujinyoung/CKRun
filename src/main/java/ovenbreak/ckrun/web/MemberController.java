dc10b0af3d58439053339a0ed5c2d382a9581cb3
jujinyoung <jujinyoung1838@naver.com>
2023-01-14 23:34:51 +0900

    feat: 로그인 페이지 제작 및 controller설정

---
+++ src/main/java/ovenbreak/ckrun/web/MemberController.java
@@ -0,0 +1,15 @@
+package ovenbreak.ckrun.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/login")
+    public String loginForm(){
+        return "member/login";
+    }
+}
